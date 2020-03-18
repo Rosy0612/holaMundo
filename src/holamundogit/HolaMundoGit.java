426699c851424b6fa674d77d4d01615317d8d16a
rosy <rosy@rosy-HP-Notebook>
2020-03-17 22:48:09 -0600

    inicio Hola Mundo para Git

--- src/holamundogit/HolaMundoGit.java
+++ src/holamundogit/HolaMundoGit.java
@@ -5,7 +5,7 @@ public class HolaMundoGit {
 
    
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        System.out.println("Hola Mundo Para Git");
     }
     
 }
